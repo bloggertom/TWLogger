dbfbcbdf6f9714bc9496bf020adafbf95adb877e
Thomas Wilson <sabre.2win@gmail.com>
2018-04-09 21:49:35 +0100

    Initial Commit

---
+++ TDWLogger/TDWLogger.h
@@ -0,0 +1,19 @@
+//
+//  TDWLogger.h
+//  TDWLogger
+//
+//  Created by Thomas Wilson on 09/04/2018.
+//  Copyright © 2018 Thomas Wilson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TDWLogger.
+FOUNDATION_EXPORT double TDWLoggerVersionNumber;
+
+//! Project version string for TDWLogger.
+FOUNDATION_EXPORT const unsigned char TDWLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TDWLogger/PublicHeader.h>
+
+
