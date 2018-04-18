ab41fc9f19b9be65e4173cfde955f6bf7c404377
Thomas Wilson <sabre.2win@gmail.com>
2018-04-18 19:30:09 +0100

    Fixed unit tests so that they can be run together without interfering with each other.

--- TDWLoggerTests/TDWFileLoggerTest.m
+++ TDWLoggerTests/TDWFileLoggerTest.m
@@ -9,10 +9,14 @@
 #import <XCTest/XCTest.h>
 #import <TDWLogger/TDWLogger.h>
 
+@interface TDWFileLogger()
+@property (nonatomic, strong)TDWLoggerOptions *options;
+-(void)stopLogging;
+@end
+
 @interface TDWFileLoggerTest : XCTestCase
-@property (nonatomic, strong)TDWFileLogger *fileLogger;
-@property (nonatomic, strong)NSString *logDirPath;
 @property (nonatomic, strong)NSFileManager *fileManager;
+@property (nonatomic, strong)NSString *baseDir;
 @end
 
 @implementation TDWFileLoggerTest
@@ -20,52 +24,65 @@
 - (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
-	_fileLogger = [[TDWFileLogger alloc]init];
-	[TDWLog addLogger:self.fileLogger];
-	
-	_logDirPath  = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0];
-	_logDirPath = [self.logDirPath stringByAppendingPathComponent:@"TDWLogFiles"];
+	_baseDir  = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0];
 	_fileManager = [NSFileManager defaultManager];
+	
 	
 }
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
-	self.fileLogger = nil;
-	[TDWLog removeLogger:self.fileLogger];
-	_fileLogger = nil;
-	
-	[[NSFileManager defaultManager]removeItemAtPath:self.logDirPath error:nil];
-	
-    [super tearDown];
+	[super tearDown];
+}
+-(NSString *)getLogDirPath{
+	return [self.baseDir stringByAppendingPathComponent:[NSString stringWithFormat:@"TestLogDir-%lld",(long long)[NSDate date].timeIntervalSince1970]];
+}
+-(void)removeLogDir:(NSString *)logDirPath{
+		[[NSFileManager defaultManager]removeItemAtPath:logDirPath error:nil];
 }
 
 -(void)testLogCreation{
 	NSString *logMessage = @"Logging Test";
-	NSError *error = nil;
-	NSArray *testDirContents = [self.fileManager contentsOfDirectoryAtPath:self.logDirPath error:&error];
 	
-	XCTAssertNotNil(error);
-	XCTAssertNil(testDirContents);
-	NSLog(logMessage);
-	
-	error = nil;
+	TDWFileLogger *fileLogger = [[TDWFileLogger alloc] init];
+	NSString *logPath = [self getLogDirPath];
+	fileLogger.options.filePath = logPath;
 
-	testDirContents = [self.fileManager contentsOfDirectoryAtPath:self.logDirPath error:&error];
+	[fileLogger logReceived:TDWLogLevelDebug body:logMessage fromFile:[NSString stringWithFormat:@"%s",__FILE__] forMethod:[NSString stringWithFormat:@"%s",__PRETTY_FUNCTION__]];
+	
+	[self logTest:logMessage logPath:logPath];
+	
+	[fileLogger stopLogging];
+	[self removeLogDir: logPath];
+}
+-(void)logTest:(NSString *)expectedResult logPath:(NSString *)logDirPath{
+	NSError *error = nil;
+	NSArray *testDirContents = [self.fileManager contentsOfDirectoryAtPath:logDirPath error:&error];
 	XCTAssertNil(error);
 	XCTAssert(testDirContents.count == 1);
 	
 	error = nil;
-	NSString *fileContents = [NSString stringWithContentsOfFile:[self.logDirPath stringByAppendingPathComponent:[testDirContents firstObject]] encoding:NSASCIIStringEncoding error:&error];
+	NSString *fileContents = [NSString stringWithContentsOfFile:[logDirPath stringByAppendingPathComponent:[testDirContents firstObject]] encoding:NSASCIIStringEncoding error:&error];
 	
-	XCTAssert([logMessage isEqualToString:[fileContents stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]]]);
-	
+	XCTAssert([expectedResult isEqualToString:fileContents]);
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testMultipleLogs{
+	TDWFileLogger *fileLogger = [[TDWFileLogger alloc]init];
+	NSString *filePath = [self getLogDirPath];
+	fileLogger.options.filePath = filePath;
+	NSMutableString *expectedResult = [[NSMutableString alloc]init];
+	NSString *format = @"Log test %d";
+	for(int i=0; i<3; i++){
+		[fileLogger logReceived:TDWLogLevelDebug body:[NSString stringWithFormat:format,i] fromFile:[NSString stringWithFormat:@"%s",__FILE__] forMethod:[NSString stringWithFormat:@"%s", __PRETTY_FUNCTION__]];
+		[expectedResult appendFormat:format, i];
+	}
+	
+	[self logTest:expectedResult logPath:filePath];
+	
+	
+	[fileLogger stopLogging];
+	[self removeLogDir:filePath];
 }
+
 
 @end
