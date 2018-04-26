16bb6ecc402a85028879248a9a676aeee3abb320
Thomas Wilson <sabre.2win@gmail.com>
2018-04-26 22:16:55 +0100

    Add log entry object which can be used to buffer log entries to an NSArray

---
+++ TWLogger/TWLogEntry.h
@@ -0,0 +1,19 @@
+//
+//  TWLogEntry.h
+//  TWLogger
+//
+//  Created by Thomas Wilson on 26/04/2018.
+//  Copyright © 2018 Thomas Wilson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "TWLogLevel.h"
+
+@interface TWLogEntry : NSObject
+
+@property (nonatomic)TWLogLevel logLevel;
+@property (nonatomic, strong)NSString *logBody;
+@property (nonatomic, strong)NSString *file;
+@property (nonatomic, strong)NSString *function;
+
+@end
---
+++ TWLogger/TWLogEntry.m
@@ -0,0 +1,13 @@
+//
+//  TWLogEntry.m
+//  TWLogger
+//
+//  Created by Thomas Wilson on 26/04/2018.
+//  Copyright © 2018 Thomas Wilson. All rights reserved.
+//
+
+#import "TWLogEntry.h"
+
+@implementation TWLogEntry
+
+@end
