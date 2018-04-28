c0ae2834a0a66e89a447f2ad6c135a19f11abbda
Thomas Wilson <sabre.2win@gmail.com>
2018-04-28 17:10:32 +0100

    Added insert method for log entries.
    - corrected date time  format
    - added column for time stamps to allow precision search of log times.
    -

--- TWLogger/TWSqlite.m
+++ TWLogger/TWSqlite.m
@@ -9,10 +9,14 @@
 #import "TWSqlite.h"
 #import <sqlite3.h>
 #import "TWLoggerErrors.h"
-#define DATE_TIME_FORMAT @"YYYY-MM-dd-HH-mm-ss O"
+#import "TWLogEntry.h"
+#import "TWUtils.h"
+
+#define DATE_TIME_FORMAT @"yyyy-MM-dd-HH-mm-ss O"
 
 NSString * const TWLogTableName = @"TWLogEntries";
 
+NSString * const TWLogTableColumnTimeStamp = @"timestamp";
 NSString * const TWLogTableColumnDateTime = @"date_time";
 NSString * const TWLogTableColumnLevel = @"level";
 NSString * const TWLogTableColumnBody = @"body";
@@ -20,6 +24,7 @@ NSString * const TWLogTableColumnFunction = @"function";
 NSString * const TWLogTableColumnFile = @"file";
 
 @interface TWSqlite ()
+
 @property (nonatomic)sqlite3 *database;
 @property (nonatomic, strong)NSDateFormatter *dateFormatter;
 @end
@@ -73,17 +78,56 @@ NSInteger databaseVersion = 1;
 }
 
 -(BOOL)createLogTable:(NSError **)error{
-	NSString *query = [NSString stringWithFormat:@"CREATE TABLE %@ IF NOT EXISTS (%@, %@, %@, %@, %@);", TWLogTableName,TWLogTableColumnDateTime, TWLogTableColumnLevel, TWLogTableColumnFile, TWLogTableColumnFunction, TWLogTableColumnBody];
+	NSString *query = [NSString stringWithFormat:@"CREATE TABLE %@ IF NOT EXISTS (%@ REAL, %@ TEXT, %@ TEXT, %@ TEXT, %@ TEXT, %@ TEXT);", TWLogTableName, TWLogTableColumnTimeStamp, TWLogTableColumnDateTime, TWLogTableColumnLevel, TWLogTableColumnFile, TWLogTableColumnFunction, TWLogTableColumnBody];
 	
 	if(sqlite3_exec(_database, query.UTF8String, NULL, NULL, NULL) != SQLITE_OK){
 		*error = [NSError errorWithDomain:ERROR_DOMAIN code:TWLoggerErrorSqliteFailedToWrite userInfo:
 				  @{NSLocalizedDescriptionKey: @"Failed write database schema"}];
 		return NO;
 	}
+	
 	return YES;
 }
 
 -(BOOL)insertEntry:(TWLogEntry *)entry error:(NSError **)error{
+	
+	NSString *query = [NSString stringWithFormat:@"INSERT INTO %@ (%@, %@, %@, %@, %@, %@) VALUES (?,?,?,?,?,?)",
+					   TWLogTableName,
+					   //columns
+					   TWLogTableColumnTimeStamp,
+					   TWLogTableColumnDateTime,
+					   TWLogTableColumnLevel,
+					   TWLogTableColumnFile,
+					   TWLogTableColumnFunction,
+					   TWLogTableColumnBody];
+	
+	sqlite3_stmt *statement;
+	@try{
+		if(sqlite3_prepare(self.database, query.UTF8String, 0, &statement, NULL) == SQLITE_OK){
+			sqlite3_bind_double(statement, 1, [entry.datetime timeIntervalSince1970]);
+			sqlite3_bind_text(statement, 2, [self.dateFormatter stringFromDate:entry.datetime].UTF8String, 0, NULL);
+			sqlite3_bind_text(statement, 3, [TWUtils logLevelString:entry.logLevel].UTF8String, 0, NULL);
+			sqlite3_bind_text(statement, 4, entry.file.UTF8String, 0, NULL);
+			sqlite3_bind_text(statement, 5, entry.function.UTF8String, 0, NULL);
+			sqlite3_bind_text(statement, 6, entry.logBody.UTF8String, 0, NULL);
+			
+			int result = sqlite3_step(statement);
+			if(result == SQLITE_OK || result == SQLITE_DONE){
+				return YES;
+			}
+		}
+	}
+	@finally{
+		if(statement){
+			sqlite3_finalize(statement);
+		}
+	}
+	
+	//getting to here means we've failed.
+	*error = [NSError errorWithDomain:ERROR_DOMAIN code:TWLoggerErrorSqliteFailedToWrite userInfo:
+				  @{NSLocalizedDescriptionKey: @"Failed to write log entry"}];
+	
+	
 	return NO;
 }
 
