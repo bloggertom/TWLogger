187b6fdb7bf79875859224a110423d90bdfc98f8
Thomas Wilson <sabre.2win@gmail.com>
2018-04-29 21:42:50 +0100

    Added SqliteLog entry object which can hold an ID for log entries.

---
+++ TWLogger/TWSqliteLogEntry.h
@@ -0,0 +1,20 @@
+//
+//  TWSqliteLogEntry.h
+//  TWLogger
+//
+//  Created by Thomas Wilson on 29/04/2018.
+//  Copyright © 2018 Thomas Wilson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "TWLogLevel.h"
+@interface TWSqliteLogEntry : NSObject
+
+@property (nonatomic)NSInteger logId;
+@property (nonatomic, strong)NSDate *datetime;
+@property (nonatomic)TWLogLevel logLevel;
+@property (nonatomic, strong)NSString *logBody;
+@property (nonatomic, strong)NSString *file;
+@property (nonatomic, strong)NSString *function;
+
+@end
---
+++ TWLogger/TWSqliteLogEntry.m
@@ -0,0 +1,13 @@
+//
+//  TWSqliteLogEntry.m
+//  TWLogger
+//
+//  Created by Thomas Wilson on 29/04/2018.
+//  Copyright © 2018 Thomas Wilson. All rights reserved.
+//
+
+#import "TWSqliteLogEntry.h"
+
+@implementation TWSqliteLogEntry
+
+@end
