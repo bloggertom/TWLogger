e0ef9339dc2b5a278a1b502233159527a580c95e
Thomas Wilson <sabre.2win@gmail.com>
2018-10-06 15:45:44 +0100

    Created skeleton class for json logger.

---
+++ TWLogger/TWJsonLogger.h
@@ -0,0 +1,15 @@
+//
+//  TWJsonLogger.h
+//  TWLogger
+//
+//  Created by Thomas Wilson on 06/10/2018.
+//  Copyright © 2018 Thomas Wilson. All rights reserved.
+//
+
+#import <TWLogger/TWLogger.h>
+
+@interface TWJsonLogger : TWAbstractLogger <TWLoggerDelegate>
+
+
+
+@end
